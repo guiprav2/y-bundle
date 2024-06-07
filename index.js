c2a8fef498f107fc16604f804f74ab3b972b833f
Gui Prá <gui@guiprav.com>
2024-06-07 00:02:13 -0300

    .

---
+++ index.js
@@ -0,0 +1,4 @@
+import * as Y from 'yjs'
+import { IndexeddbPersistence } from 'y-indexeddb';
+import { WebsocketProvider } from 'y-websocket';
+export { Y, IndexeddbPersistence, WebsocketProvider };
